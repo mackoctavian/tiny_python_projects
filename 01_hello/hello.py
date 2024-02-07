53ed0710916e816face4c5ca872dd6f550ae95ef
Mack Octavian <maestrooctavian@gmail.com>
2024-02-07 13:17:03 +0300

    Added a program that print a greet based on the commandline argument

--- 01_hello/hello.py
+++ 01_hello/hello.py
@@ -1,3 +1,17 @@
 #!/usr/bin/env python3
 # Purpose: Say Hello
-print('Hello, World!')
+import argparse
+
+def get_args():
+    parser = argparse.ArgumentParser(description="Say Hello")
+    parser.add_argument('-n', '--name', help="Name to greet", metavar='name', default='World')
+    return parser.parse_args()
+
+
+
+def main():
+    args = get_args()
+    print('Hello, ' + args.name + '!')
+
+if __name__ == '__main__':
+    main()
