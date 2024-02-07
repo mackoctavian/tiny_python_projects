844b8d08463590d8d19deb6a178fc8d47af05107
Mack Octavian <maestrooctavian@gmail.com>
2024-02-07 13:29:15 +0300

    Added docstring for our program and functions

--- 01_hello/hello.py
+++ 01_hello/hello.py
@@ -1,17 +1,29 @@
 #!/usr/bin/env python3
-# Purpose: Say Hello
+"""
+Author: Mack Octavian <maestrooctavian@gmail.com"
+Purpose: Say Hello
+"""
 import argparse
 
+
 def get_args():
+    """Get the command-line arguments """
+
     parser = argparse.ArgumentParser(description="Say Hello")
-    parser.add_argument('-n', '--name', help="Name to greet", metavar='name', default='World')
+    parser.add_argument('-n',
+                        '--name',
+                        help="Name to greet",
+                        metavar='name',
+                        default='World')
     return parser.parse_args()
 
 
-
 def main():
+    """Make a jazz noise here"""
+
     args = get_args()
     print('Hello, ' + args.name + '!')
+
 
 if __name__ == '__main__':
     main()
