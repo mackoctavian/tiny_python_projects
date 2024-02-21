821f45b2e4c1163ebcc5ea151e7de97ae6f23696
Mack Octavian <maestrooctavian@gmail.com>
2024-02-22 00:22:14 +0300

    Added -l flag to print in lower case

--- 05_howler/howler.py
+++ 05_howler/howler.py
@@ -10,15 +10,18 @@ import os
 
 
 def get_args():
-    parser = argparse.ArgumentParser(description="Howler (upper-cases input)", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument('text',  help='Input string or file')
+    parser = argparse.ArgumentParser(
+        description="Howler (upper-cases input)",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('text', help='Input string or file')
     parser.add_argument('-o',
                         '--outfile',
                         metavar='str',
                         type=str,
                         help='Output filename',
                         default='')
-
+    parser.add_argument('-l',
+                        '--lower', help="Lower the input text", action='store_true')
     return parser.parse_args()
 
 
@@ -26,14 +29,20 @@ def main():
     args = get_args()
     word = args.text
     out = args.outfile
+    lower1 = args.lower
 
     if os.path.isfile(word):
         word = open(word).read().upper().rstrip()
-        print(word)
+        if lower1:
+            print(word.lower())
+        else:
+            print(word)
     elif out:
         fh = open(out, 'wt')
         fh.write(word.upper().rstrip())
         fh.close()
+    elif lower1:
+        print(word.lower())
     else:
         print(word.upper())
 
