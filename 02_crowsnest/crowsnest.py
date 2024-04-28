ef7aaff5edc91418bf7c20aa9c3dc56f66e88846
Mack Octavian <maestrooctavian@gmail.com>
2024-04-28 21:58:54 +0300

    Revision

--- 02_crowsnest/crowsnest.py
+++ 02_crowsnest/crowsnest.py
@@ -1,23 +1,40 @@
 #!/usr/bin/env python3
 """
-Author : Mack Octavian <maestrooctavian@gmail.com>
-Date   : 2024-02-07
+Author : whisperer <whisperer@localhost>
+Date   : 2024-04-28
 Purpose: Rock the Casbah
 """
 
 import argparse
 
 
+# --------------------------------------------------
 def get_args():
-    parser = argparse.ArgumentParser(description='Rock the Casbah')
-    parser.add_argument('word', metavar='word', help='A word')
+    """Get command-line arguments"""
+
+    parser = argparse.ArgumentParser(
+        description="Crow's Nest -- choose the correct article",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument('word',
+                        metavar='word',
+                        help='A word')
+
+
+
     return parser.parse_args()
 
+
+# --------------------------------------------------
 def main():
+    """Make a jazz noise here"""
+
     args = get_args()
     word = args.word
     article = 'an' if word[0].lower() in 'aeiou' else 'a'
-    print(f'Ahoy, Captain, {article} {word} off the larboard bow!')  
+    print(f'Ahoy, Captain, {article} {word} off the larboard bow!')
 
+
+# --------------------------------------------------
 if __name__ == '__main__':
     main()
